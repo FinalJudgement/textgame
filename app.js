1791d58c49ae3e34b5f4994a7e34d69dbcf86567
Carlos Silva <silvacj90@gmail.com>
2022-09-12 22:29:04 -0500

    finally able to select race

--- app.js
+++ app.js
@@ -1,119 +1,317 @@
-const contents = ['intro', 'creation'];
-const launch = document.querySelector('#launch');
-const dialogue = document.querySelector('#dialogue');
-const choiceBtns = document.querySelectorAll('.choices');
-const option1 = document.querySelector('#option1');
-const option2 = document.querySelector('#option2');
-const option3 = document.querySelector('#option3');
-let chapter = 0;
-let counter = 1;
-let count = counter.toString();
+//These variables are used to grab an element for manipulation.
+const launchButtonElement = document.querySelector('#launch');
+const dialogueElement = document.querySelector('#dialogue');
+const choiceButtonElements = document.querySelector('#choices');
+const choiceBtnsElement = document.querySelectorAll('.choice');
 
-// function startGame(1){
+
+let characterState = {};
+
+function startGame() {
+    characterState = {};
+    showDialogue(1);
+}
+
+function showDialogue(storyNodeIndex) {
+    const storyNode = story.find(storyNodeDialogue => storyNodeDialogue.id === storyNodeIndex)
+    dialogueElement.innerText = storyNode.dialogue
+
+    while (choiceButtonElements.firstChild) {
+        choiceButtonElements.removeChild(choiceButtonElements.firstChild);
+    }
+
+    storyNode.options.forEach((option) => {
+        if (showOption(option)) {
+            const button = document.createElement('button');
+            button.innerText = option.text
+            button.classList.add('choice')
+            button.addEventListener('click', () => selectOption(option))
+            choiceButtonElements.appendChild(button);
+        } else {
+            const button = document.createElement('button');
+            button.innerText = option.text
+            button.classList.add('choice')
+            button.addEventListener('click', () => failedRequirement(option))
+            choiceButtonElements.appendChild(button);
+        }
+    })
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(characterState);
+}
+function selectOption(option) {
+    const nextStoryNode = option.nextText;
+    characterState = Object.assign(characterState, option.setState)
+    showDialogue(nextStoryNode)
+}
+function failedRequirement(option) {
+    dialogueElement.innerHTML = option.requirementText;
+}
+
+launchButtonElement.addEventListener('click', () => {
+    for (let ea of choiceBtnsElement) { ea.classList.toggle('invisible'); }
+    launchButtonElement.classList.toggle('invisible');
+    startGame();
+})
+
+// // this adds and event listener to wait for the launch button click
+// launchButtonElement.addEventListener('click', () => {
+//     //this for of loop is simply causing the 3 choice buttons on the bottom to appear
+//     for (let ea of choiceBtnsElement) { ea.classList.toggle('invisible'); }
+//     //This toggle simply makes the launch button invisible.
+//     launchButtonElement.classList.toggle('invisible');
+//     dialogueElement.innerText = story[contents[chapter]]['step' + count]['dia']
+//     optionElement1.innerText = story[contents[chapter]]['step' + count]['option1']['text'];
+//     optionElement2.innerText = story[contents[chapter]]['step' + count]['option2']['text'];
+//     optionElement3.innerText = story[contents[chapter]]['step' + count]['option3']['text'];
+
+//     counter++
+//     count = counter.toString();
+// })
+
+// option2.addEventListener('click', () => {
+//     if (story[contents[chapter]]['step' + count] === 'next') {
+//         chapter++
+//         counter = 1
+//     }
+//     dialogueElement.innerText = story[contents[chapter]]['step' + count]['dia']
+//     optionElement1.innerText = story[contents[chapter]]['step' + count]['option1']['text'];
+//     optionElement2.innerText = story[contents[chapter]]['step' + count]['option2']['text'];
+//     optionElement3.innerText = story[contents[chapter]]['step' + count]['option3']['text'];
+//     counter++
+//     count = counter.toString();
+
+// })
+
+const story = [
+    {
+        id: 1,
+        dialogue: 'It is night fall. After a long journey through the country of purus, after fighting off fiends and enemies alike you find yourself tired, famished and low on water. You see a dimly lit town in the distance and decide to seek shelter for the night.',
+        options: [
+            {
+
+                text: 'disabled'
+            },
+            {
+                text: 'Enter The Town',
+                nextText: 2
+            },
+            {
+                text: 'disabled'
+            }
+        ]
+    },
+    {
+        id: 2,
+        dialogue: 'As you enter the town, you see what looks like a tavern. You say to yourself "Thank the heavens". as you stumble up the steps, you walk through the door. as you walk up to the bar, you see a fair maiden serving tables and can feel the gaze of the patrons upon you.',
+        options: [
+            {
+
+                text: 'disabled'
+            },
+            {
+                text: 'Sit at the bar',
+                nextText: 3
+            },
+            {
+                text: 'disabled'
+            }
+        ]
+    },
+    {
+        id: 3,
+        dialogue: 'The bartender walks toward you, "woah, you look Horrible, let me get you drink, here it\'s on the house." so tell me, where are you from stranger?',
+        options: [
+            {
+
+                text: 'disabled'
+            },
+            {
+                text: 'Choose Your Race',
+                nextText: 4
+            },
+            {
+                text: 'disabled'
+            }
+        ]
+    },
+    {
+        id: 4,
+        dialogue: 'The Famours Raven Trainers, Home to some of the worlds most lethal assassins. Masters of the shadow and stealth arts. This tribe relies on Agility to take down their foes as quick as possible, while mitigating the damage they receive.',
+        options: [
+            {
+
+                text: 'previous',
+                nextText: 6
+            },
+            {
+                text: 'Select Kage Clan',
+                setState: { assassin: true },
+                nextText: 7
+
+            },
+            {
+                text: 'next',
+                nextText: 5
+            }
+        ]
+    },
+    {
+        id: 5,
+        dialogue: 'A clan of Tough warriors, with elite strength and defensive abilities, they use their battle cries to instill fear in their enemies.',
+        options: [
+            {
+
+                text: 'previous',
+                nextText: 4
+            },
+            {
+                text: 'Select Tusk Clan!',
+                setState: { warrior: true },
+                nextText: 7
+            },
+            {
+                text: 'next',
+                nextText: 6
+            }
+        ]
+    },
+    {
+        id: 6,
+        dialogue: 'TBD',
+        options: [
+            {
+
+                text: 'previous',
+                nextText: 5
+            },
+            {
+                text: 'Coming Soon!',
+                // nextText: 7
+            },
+            {
+                text: 'next',
+                nextText: 4
+            }
+        ]
+    },
+    {
+        id: 7,
+        dialogue: 'Welcome Warrior to Text Game Online',
+        options: [
+            {
+
+                text: 'disabled',
+
+            },
+            {
+                text: 'Step Forth',
+                nextText: 8
+            },
+            {
+                text: '(assassin) pick pocket the bartender',
+                requirementText: 'You do not meet the requirement for this choice',
+                requiredState: (currentState) => currentState.assassin,
+                nextText: 8
+            }
+        ]
+    },
+    {
+        id: 8,
+        dialogue: 'test',
+        options: [
+            {
+
+                text: 'disabled',
+                // nextText: 6
+            },
+            {
+                text: 'disabled',
+                // nextText: 7
+            },
+            {
+                text: 'disabled',
+                // nextText: 5
+            }
+        ]
+    }
+];
+
+
+// const story = {
+//     intro: {
+
+//         step1: {
+//             dia: 'It is night fall. After a long journey through the country of purus, after fighting off fiends and enemies alike you find yourself tired, famished and low on water. You see a dimly lit town in the distance and decide to seek shelter for the night.',
+//             option1: {
+//                 text: 'disabled'
+
+//             },
+//             option2: {
+//                 text: 'Enter The Town',
+//                 value: 'proceed'
+
+//             },
+//             option3: {
+//                 text: 'disabled'
+//             }
+//         },
+//         step2: {
+//             dia: 'As you enter the town, you see what looks like a tavern. You say to yourself "Thank the heavens". as you stumble up the steps, you walk through the door. as you walk up to the bar, you see a fair maiden serving tables and can feel the gaze of the patrons upon you.',
+//             option1: {
+//                 text: 'disabled',
+//                 value: 1
+//             },
+//             option2: {
+//                 text: 'Sit at the bar',
+//                 value: 'proceed'
+//             },
+//             option3: {
+//                 text: 'disabled',
+//                 value: 3
+//             }
+//         },
+//         step3: {
+//             dia: 'The bartender walks toward you, "woah, you look horrid, let me get you drink, here it\'s on the house." so tell me, where are you from stranger?',
+//             option1: {
+//                 text: 'disabled',
+//                 value: 1
+//             },
+//             option2: {
+//                 text: 'Choose Your Race',
+//                 value: 'proceed'
+//             },
+//             option3: {
+//                 text: 'disabled',
+//                 value: 3
+//             }
+//         },
+//         step4: 'next'
+
+//     },
+//     creation: {
+//         step1: {
+//             dia: 'Home to some of the worlds most lethal assassins. Masters of the shadow and stealth arts. This tribe relies on Agility to take down their foes as quick as possible, while mitigating the damage they receive.',
+//             option1: 'prev',
+//             option2: 'The Raven Tribe',
+//             option3: 'next'
+//         },
+//         step2: {
+//             dia: '',
+//             option1: '',
+//             option2: '',
+//             option3: ''
+//         },
+//         step3: {
+//             dia: '',
+//             option1: '',
+//             option2: '',
+//             option3: ''
+//         }
+//     }
 
 // }
-
-launch.addEventListener('click', () => {
-    for (let ea of choiceBtns) { ea.classList.toggle('invisible'); }
-    launch.classList.toggle('invisible');
-    dialogue.innerText = story[contents[chapter]]['step' + count]['dia']
-    option1.innerText = story[contents[chapter]]['step' + count]['option1']['text'];
-    option2.innerText = story[contents[chapter]]['step' + count]['option2']['text'];
-    option3.innerText = story[contents[chapter]]['step' + count]['option3']['text'];
-
-    counter++
-    count = counter.toString();
-})
-
-option2.addEventListener('click', () => {
-    if (story[contents[chapter]]['step' + count] === 'next') {
-        chapter++
-        counter = 1
-    }
-    dialogue.innerText = story[contents[chapter]]['step' + count]['dia']
-    option1.innerText = story[contents[chapter]]['step' + count]['option1']['text'];
-    option2.innerText = story[contents[chapter]]['step' + count]['option2']['text'];
-    option3.innerText = story[contents[chapter]]['step' + count]['option3']['text'];
-    counter++
-    count = counter.toString();
-
-})
-
-
-
-const story = {
-    intro: {
-
-        step1: {
-            dia: 'It is night fall. After a long journey through the country of purus, after fighting off fiends and enemies alike you find yourself tired, famished and low on water. You see a dimly lit town in the distance and decide to seek shelter for the night.',
-            option1: {
-                text: 'disabled'
-
-            },
-            option2: {
-                text: 'Enter The Town',
-                value: 'proceed'
-
-            },
-            option3: {
-                text: 'disabled'
-            }
-        },
-        step2: {
-            dia: 'As you enter the town, you see what looks like a tavern. You say to yourself "Thank the heavens". as you stumble up the steps, you walk through the door. as you walk up to the bar, you see a fair maiden serving tables and can feel the gaze of the patrons upon you.',
-            option1: {
-                text: 'disabled',
-                value: 1
-            },
-            option2: {
-                text: 'Sit at the bar',
-                value: 'proceed'
-            },
-            option3: {
-                text: 'disabled',
-                value: 3
-            }
-        },
-        step3: {
-            dia: 'The bartender walks toward you, "woah, you look horrid, let me get you drink, here it\'s on the house." so tell me, where are you from stranger?',
-            option1: {
-                text: 'disabled',
-                value: 1
-            },
-            option2: {
-                text: 'Choose Your Race',
-                value: 'proceed'
-            },
-            option3: {
-                text: 'disabled',
-                value: 3
-            }
-        },
-        step4: 'next'
-
-    },
-    creation: {
-        step1: {
-            dia: 'Home to some of the worlds most lethal assassins. Masters of the shadow and stealth arts. This tribe relies on Agility to take down their foes as quick as possible, while mitigating the damage they receive.',
-            option1: 'prev',
-            option2: 'The Raven Tribe',
-            option3: 'next'
-        },
-        step2: {
-            dia: '',
-            option1: '',
-            option2: '',
-            option3: ''
-        },
-        step3: {
-            dia: '',
-            option1: '',
-            option2: '',
-            option3: ''
-        }
-    }
-
-}
 //const charTribe = {
     //     raven: 'The Raven Tribe',
     //     description: 'Home to some of the worlds most lethal assassins. Masters of the shadow and stealth arts. This tribe relies on Agility to take down their foes as quick as possible, while mitigating the damage they receive.'
